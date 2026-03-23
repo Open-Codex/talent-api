b67e052c40e6a89e2f50cffe78d38f7fcb70d273
Felipe Castillo <felipecastillo.snk@gmail.com>
2026-03-23 19:29:02 -0300

    feat: add more filters for developer

--- src/modules/developer/dto/query-developers.dto.ts
+++ src/modules/developer/dto/query-developers.dto.ts
@@ -1,5 +1,6 @@
 import { Type } from 'class-transformer';
-import { IsOptional, IsInt, Min, Max } from 'class-validator';
+import { IsOptional, IsInt, Min, Max, IsEnum, IsString } from 'class-validator';
+import { DeveloperStatus, Seniority } from '@prisma/client';
 
 export class QueryDeveloperDto {
 	@IsOptional()
@@ -14,4 +15,20 @@ export class QueryDeveloperDto {
 	@Min(1)
 	@Max(50)
 	limit?: number = 10;
+
+	@IsOptional()
+	@IsString()
+	stack?: string;
+
+	@IsOptional()
+	@IsString()
+	location?: string;
+
+	@IsOptional()
+	@IsEnum(Seniority)
+	seniority?: Seniority;
+
+	@IsOptional()
+	@IsEnum(DeveloperStatus)
+	status?: DeveloperStatus;
 }
